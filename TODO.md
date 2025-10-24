c305fa46430ce7754a1b7023b8b4bce7545e923e
appakling <henryallenberman@gmail.com>
2025-10-24 09:18:09 -0500

    everything

--- TODO.md
+++
@@ -1,7 +0,0 @@
-# TO DO
-1. ~~get all flash files~~ **DONE**
-2. ~~get all pico-8 files~~ **DONE**
-3. add all games wanted
-4. finish UI
-5. polishing 
-6. extra features
--- data/fullscreen.js
+++
@@ -1,9 +0,0 @@
-$('#toggle_fullscreen').on('click', function(){
-  // if already full screen; exit
-  // else go fullscreen
-  if (document.fullscreenElement) {
-    document.exitFullscreen();
-  } else {
-    $('#container').get(0).requestFullscreen();
-  }
-});
